5e6e7937c8a2ffdf17df287b1be2dda898f06b56
Serban Marinescu <serban.2000@yahoo.com>
2025-12-31 17:20:51 +0200

    Add Spring Boot Service Convention Plugin

---
+++ chirp/build-logic/src/main/kotlin/chirp.spring-boot-service.gradle.kts
@@ -0,0 +1,24 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+
+plugins {
+    id("chirp.kotlin-common")
+    kotlin("plugin.jpa")
+    id("io.spring.dependency-management")
+}
+
+configure<DependencyManagementExtension> {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:${libraries.findVersion("spring-boot").get()}")
+    }
+}
+
+dependencies {
+    "implementation"(libraries.findLibrary("kotlin-reflect").get())
+    "implementation"(libraries.findLibrary("kotlin-stdlib").get())
+    "implementation"(libraries.findLibrary("spring-boot-starter-web").get())
+
+    "testImplementation"(libraries.findLibrary("spring-boot-starter-test").get())
+    "testImplementation"(libraries.findLibrary("kotlin-test-junit5").get())
+
+    "testRuntimeOnly"(libraries.findLibrary("junit-platform-launcher").get())
+}
