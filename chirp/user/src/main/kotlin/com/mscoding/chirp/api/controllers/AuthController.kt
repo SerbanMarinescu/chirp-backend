d84aa908335e13f674bd0af6a8134da2ad720802
Serban Marinescu <serban.2000@yahoo.com>
2026-02-03 08:34:05 +0200

    Implement Login Endpoint

--- chirp/user/src/main/kotlin/com/mscoding/chirp/api/controllers/AuthController.kt
+++ chirp/user/src/main/kotlin/com/mscoding/chirp/api/controllers/AuthController.kt
@@ -1,7 +1,10 @@
 package com.mscoding.chirp.api.controllers
 
+import com.mscoding.chirp.api.dto.AuthenticatedUserDto
+import com.mscoding.chirp.api.dto.LoginRequest
 import com.mscoding.chirp.api.dto.RegisterRequest
 import com.mscoding.chirp.api.dto.UserDto
+import com.mscoding.chirp.api.mappers.toAuthenticatedUserDto
 import com.mscoding.chirp.api.mappers.toUserDto
 import com.mscoding.chirp.service.auth.AuthService
 import jakarta.validation.Valid
@@ -25,5 +28,15 @@ class AuthController(
             username = body.username,
             password = body.password
         ).toUserDto()
+    }
+
+    @PostMapping("/login")
+    fun login(
+       @RequestBody body: LoginRequest
+    ): AuthenticatedUserDto {
+        return authService.login(
+            email = body.email,
+            password = body.password
+        ).toAuthenticatedUserDto()
     }
 }
--- chirp/user/src/main/kotlin/com/mscoding/chirp/api/dto/LoginRequest.kt
+++ chirp/user/src/main/kotlin/com/mscoding/chirp/api/dto/LoginRequest.kt
@@ -1,6 +1,10 @@
 package com.mscoding.chirp.api.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class LoginRequest(
+    @JsonProperty("email")
     val email: String,
+    @JsonProperty("password")
     val password: String
 )
